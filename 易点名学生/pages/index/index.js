d928a2c6c31b97caaa2078ec40ca5286a0d472ac
unknown <lihuiocean@hotmail.com>
2022-11-17 19:15:15 +0800

    完善消息界面

--- 易点名学生/pages/index/index.js
+++ 易点名学生/pages/index/index.js
@@ -3,12 +3,18 @@
 Page({
   data: {
     show:false,
+    show1:false,
     showLeave:false
   },
   //当点击课程按钮时发生的事件
   choose:function(e){
     this.setData({
       show:!this.data.show//点击按钮之后将show置为true
+    })
+  },
+  choose1:function(e){
+    this.setData({
+      show1:!this.data.show1//点击按钮之后将show1置为true
     })
   },
   //当点击请假按钮时发生的事件
@@ -29,7 +35,8 @@ Page({
   },
   back(){
     this.setData({
-      show:false/*让弹窗消失*/
+      show:false,/*让弹窗消失*/
+      show1:false
     })
   },
   bindViewTap() {
